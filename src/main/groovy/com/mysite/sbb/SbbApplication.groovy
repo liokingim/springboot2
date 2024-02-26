a33f4ff10e209516eccbe07257cefa9a277daba5
liokingim <liokingim@gmail.com>
2024-02-27 00:37:00 +0900

    테스트2

---
+++ src/main/groovy/com/mysite/sbb/SbbApplication.groovy
@@ -0,0 +1,13 @@
+package com.mysite.sbb
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SbbApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SbbApplication, args)
+	}
+
+}
---
+++ src/main/groovy/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,32 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+    	List<Question> questions = this.questionRepository.findAll();
+    	
+    	return questions;
+    }
+    
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
